81b0d35d027100d9e001b4bb2c9d1a037d1da908
Gustavo Covas <gtcovas@gmail.com>
2017-06-08 23:21:48 -0300

    Remove warnings

--- src/crypto-algorithms/base64_test.cu
+++ src/crypto-algorithms/base64_test.cu
@@ -23,7 +23,6 @@ int base64_test()
     BYTE code[1024] = {"TWFuIGlzIGRpc3Rpbmd1aXNoZWQsIG5vdCBvbmx5IGJ5IGhpcyByZWFzb24sIGJ1dCBieSB0aGlz\nIHNpbmd1bGFyIHBhc3Npb24gZnJvbSBvdGhlciBhbmltYWxzLCB3aGljaCBpcyBhIGx1c3Qgb2Yg\ndGhlIG1pbmQsIHRoYXQgYnkgYSBwZXJzZXZlcmFuY2Ugb2YgZGVsaWdodCBpbiB0aGUgY29udGlu\ndWVkIGFuZCBpbmRlZmF0aWdhYmxlIGdlbmVyYXRpb24gb2Yga25vd2xlZGdlLCBleGNlZWRzIHRo\nZSBzaG9ydCB2ZWhlbWVuY2Ugb2YgYW55IGNhcm5hbCBwbGVhc3VyZS4="};
     BYTE buf[1024];
     size_t buf_len;
-    size_t buf_len_null;
     size_t* d_buf_len;
     size_t* d_buf_len_null;
     int pass = 1;
